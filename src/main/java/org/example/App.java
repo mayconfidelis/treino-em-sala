9f975ec517aef436532833b475effc62ddf493ec
mayconfidelis <mayconfidelis@gmail.com>
2025-03-06 20:48:59 -0300

    Branch de soma

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,14 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static int soma (int a, int b){
+        return a + b;
+    }
+    public static void main( String[] args) {
+
+        int resultado = soma (10, 12);
+        System.out.println("A soma dos dois números é: " + resultado);
     }
 }
