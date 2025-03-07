0fad804a9cee7967e12cdf74e214d215dcedb589
mayconfidelis <mayconfidelis@gmail.com>
2025-03-06 21:59:41 -0300

    branch de fatoração

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,8 +6,22 @@ package org.example;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static long calcularFatorial (int numero){
+       if (numero <0) {
+           throw new IllegalArgumentException("O número deve ser positivo.");
+       }
+        long fatorial = 1;
+       for (int i = 2; i <= numero; i++) {
+           fatorial *= i;
+       }
+       return fatorial;
+    }
+    public static void main( String[] args ) {
+
+        int numero = 5;
+        long resultado = calcularFatorial(numero);
+        System.out.println("O fatorial de " + numero + " é " + resultado);
+
+
     }
 }
