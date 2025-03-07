7bf552afe8cd96f1884374dc2dd85ce33c1da81e
mayconfidelis <mayconfidelis@gmail.com>
2025-03-06 21:19:50 -0300

    branch de par ou impar

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,21 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static String parImpar (int numero){
+        if (numero%2==0){
+            return "Par";
+        } else {
+            return "Impar";
+        }
+    }
+
+    public static void main( String[] args ) {
+        int numero = 7;
+
+        String resultado = parImpar(numero);
+
+        System.out.println("O número " + numero + " é " + resultado);
     }
 }
